af1475f94786375cb7076493ea9690d895b4559d
NarrowSpace <zhuwentianzz@gmail.com>
2023-08-10 22:54:05 -0400

    Add Hand Interaction

--- V54Backup/Assets/_MyFolder/HueLightManager.cs
+++ V54Backup/Assets/_MyFolder/HueLightManager.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using MiniJSON;
+using System.Net;
+using System.Linq;
 
 public class HueLightManager : MonoBehaviour
 {
@@ -21,20 +26,21 @@ public class HueLightManager : MonoBehaviour
 
     private bool bubbleAllSet = false;
 
-    public GameObject bubbleGuideInfo;
-    public GameObject bubbleMenu; // Menu displays when the bubbles are set up
-
     private int bubbleCounter = 0;
-    private TextMeshProUGUI bubbleNumber;
     private LaserState currentLaserState = LaserState.NotHitting;
 
     private GameObject lastHitBubble = null;
 
+    public Camera centralEyeCam;
+    public GameObject setupInfo;
+
+    private List<TextMeshProUGUI> allTextMeshes  = new List<TextMeshProUGUI>();
+
+
     private void Awake()
     {
-        bubbleGuideInfo.SetActive(true);
-        bubbleMenu.SetActive(false);
-        bubbleNumber = bubbleMenu.GetComponentInChildren<TextMeshProUGUI>();
+        // This will get all the TextMeshProUGUI components that are children of setupInfo.
+        allTextMeshes = setupInfo.GetComponentsInChildren<TextMeshProUGUI>().ToList();
     }
 
     void Start()
@@ -45,13 +51,8 @@ public class HueLightManager : MonoBehaviour
 
     void Update()
     {
-        SetUpBubble();
-        SetupFinished();
-    }
+        UserMenu();
 
-
-    void SetUpBubble()
-    {
         // 1. Get the Thumbstick Input and Adjust the Laser Length:
         thumbstickVal = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y;
         float changeFactor = 0.05f;  // Control how much Input Value changes maxPointerDistance
@@ -59,12 +60,10 @@ public class HueLightManager : MonoBehaviour
         float minDistance = 0.001f;
         maxPointerDistance = Mathf.Max(maxPointerDistance, minDistance);
 
-
         // 2. Setting Laser Position:
         Vector3 endPoint = targetSpawnPoint.position + targetSpawnPoint.forward * maxPointerDistance;
         _lineRender.SetPosition(0, targetSpawnPoint.position);
         _lineRender.SetPosition(1, endPoint);
-
 
         // 3. Bubble Positioning:
         bubbleOnEndPoint.transform.position = endPoint;
@@ -77,41 +76,27 @@ public class HueLightManager : MonoBehaviour
             // Increment the bubble counter
             bubbleCounter++;
 
-            // Debug.Log(bubbleCounter);
-
             newBubble.name = "Bubble" + bubbleCounter;
         }
-    }
 
-    void SetupFinished()
-    {
         if (OVRInput.GetDown(OVRInput.Button.Two))
         {
             bubbleAllSet = true;
             bubbleOnEndPoint.SetActive(false);
         }
 
-
+        // Check for hitting a bubble continuously after the bubbles are set.
         if (bubbleAllSet)
         {
-            ShowMenu();
-            currentLaserState = LaserState.NotHitting;
-            bubbleNumber.text = "Choose a Light to Turn ON/OFF";
-
             if (Physics.Raycast(targetSpawnPoint.position, targetSpawnPoint.forward, out hitInfo, maxPointerDistance))
             {
                 if (hitInfo.collider.gameObject.CompareTag("Bubble"))
                 {
                     currentLaserState = LaserState.HittingBubble;
                     _lineRender.SetPosition(1, hitInfo.point); // Set the ray's endpoint to where it hit the bubble
-                    // Debug.Log("I HIT " + hitInfo.collider.gameObject.name);
+                    Debug.Log("I HIT " + hitInfo.collider.gameObject.name);
 
-                    // Extracts the 6th character which is the number from the name like "Bubble1", "Bubble2"...
-                    bubbleNumber.text = " Press 'A' to Turn ON/OFF Light " + hitInfo.collider.gameObject.name.Substring(6);
-
-                    // Turn On the Point Light:
-
-                    // If there is bubble that were previously hit, turn off its light
+                    // If there is a bubble that was previously hit, turn off its light
                     if (lastHitBubble != null)
                     {
                         lastHitBubble.transform.GetChild(0).GetComponent<Light>().enabled = false;
@@ -120,25 +105,36 @@ public class HueLightManager : MonoBehaviour
                     // if hit, Update the lastHitBubble and turn on the light
                     lastHitBubble = hitInfo.collider.gameObject;
                     lastHitBubble.transform.GetChild(0).GetComponent<Light>().enabled = true;
-
                 }
             }
 
-            else if (lastHitBubble != null) // if the bubble didnot hit anything
+            else if (lastHitBubble != null) // if the bubble did not hit anything
             {
                 // If laser is not hitting any bubble, turn off the light of the last hit bubble
                 lastHitBubble.transform.GetChild(0).GetComponent<Light>().enabled = false;
                 lastHitBubble = null; // Reset the last hit bubble reference
             }
+
+            // Example: change the text of the first TextMeshProUGUI component (if it exists)
+            if (allTextMeshes.Count > 0)
+            {
+                allTextMeshes[0].text = "First Test!!";
+            }
         }
+
     }
 
-
-    void ShowMenu()
+    void UserMenu()
     {
-        bubbleGuideInfo.SetActive(false);
-        bubbleMenu.SetActive(true);
-    }
+        // Get the position of the right controller and add a slight upward offset.
+        Vector3 menuPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch) + Vector3.up * 0.02f;
 
+        // Set the position of setupInfo canvas to the calculated position.
+        setupInfo.transform.position = menuPosition;
+
+        // Adjust the rotation of the setupInfo canvas to face the main camera.
+        setupInfo.transform.rotation = Quaternion.LookRotation(menuPosition - centralEyeCam.transform.position);
+
+    }
 
 }
